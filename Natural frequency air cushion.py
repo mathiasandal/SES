cb825eb4bad0b110f7c4082e04fec110a65206a7
mathiasandal <mathias.andal@gmail.com>
2021-09-09 12:30:59 +0200

    Finished script for calculating dynamic properties of an air cushion with given input parameters.

---
+++ Natural frequency air cushion.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+"""Input parameters"""
+A_b = 60  # [m^2] Air cushion area
+h_b = 0.1  # [m] Distance from waterline to the roof of the air cushion
+M = 25e3  # [kg] Total mass of the vessel
+p_0 = 3500  # [Pa] Excess air pressure in air cushion
+p_a = 101325  # [Pa] Atmospheric pressure
+gamma = 1.4  # [-] Ratio of specific heat for air
+Q_0 = 50  # [m^3/s] Mean volumetric flow of air fan
+dQdp_0 = -7.782e-3  # [(m^3s^-1)/(Pa)] Slope of fan characteristic curve estimated from Figure 5.6 in Faltinsen High-Speed
+
+'''
+Equivalent mass, damping and spring coefficients
+m*ddx + c*dx + k*x = F(t)
+'''
+m = M*h_b/gamma/(p_0 + p_a)  # [m^2 s^2] Mass equivalent coefficient
+c = M/A_b/p_0 * (0.5*Q_0 - dQdp_0*p_0)  # [m^2 s] Damping equivalent coefficient
+k = A_b  # [m^2] Stiffness equivalent coefficient
+
+'''Calculating natural frequency and damping ratio'''
+
+omega_0 = np.sqrt(k/m)  # [rad/s] Natural frequency of uniform pressure oscillations
+c_cr = 2 * np.sqrt(k*m)  # [m^2 s] Critical damping of the system
+zeta = c/c_cr  # [-] Damping ratio of the system
+
