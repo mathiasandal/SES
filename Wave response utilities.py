401ed1122eb91dc15612fa009a2103a341d003a2
mathiasandal <mathias.andal@gmail.com>
2021-09-20 11:51:58 +0200

    Created new file to contain utility functions. Created and implemented solve_eq_motion_steady_state(...)

---
+++ Wave response utilities.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def solve_eq_motion_steady_state(M, B, C, F, omega):
+    """
+    Solves the steady state solution for a given frequency.
+
+    :param M: Mass matrix, nxn numpy array
+    :param B: Damping matrix, nxn numpy array
+    :param C: Restoring matrix, nx1 numpy array
+    :param F: Excitation amplitudes, nx1 numpy array
+    :param omega: [rad/s] excitation frequency
+    :return: eta: response amplitudes
+    """
+    eta = np.linalg.solve(-omega**2 * M + 1j*omega * B + C, F)
+
+    return eta
