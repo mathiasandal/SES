09352fc018147542c619be9da01f4724d9aa3e0d
mathiasandal <mathias.andal@gmail.com>
2021-10-05 15:33:03 +0200

    Created and implemented read_re8_file(...)

---
+++ read_re8_file.py
@@ -0,0 +1,57 @@
+import numpy as np
+"""
+Reads a *.re8 file created by VERES
+
+A description of a *.re8 file is found in page 116 of the ShipX Vessel Responses (VERES) User's manual.
+
+Only works for High-Speed formulation in VERES
+"""
+
+
+def read_re8_file(filename):
+    f = open(filename, 'r')
+
+    for i in range(6):  # skips first six lines
+        f.readline()
+
+    # Read in parameters
+    RHOSW, GRAV = [float(i) for i in f.readline().split()]
+    LPP, BREADTH, DRAUGHT = [float(i) for i in f.readline().split()]
+    LCG, VCG = [float(i) for i in f.readline().split()]
+
+    # Read in number of velocities, headings, frequencies and degrees of freedom
+    NOVEL, NOHEAD, NOFREQ, NDOF = [int(i) for i in f.readline().split()]
+
+
+    # Initialize vectors to contain velocities, headings and frequencies
+    VEL = np.zeros(NOVEL)  # [m/s] Velocity of the vessel
+    HEAD = np.zeros(NOHEAD)  # [deg] Wave heading
+    FREQ = np.zeros(NOFREQ)  # [rad/s] Wave frequency
+
+    # Initialize values to be stores for each velocity
+    SINK = np.zeros(NOVEL)  # [m] Sink
+    TRIM = np.zeros(NOVEL)  # [deg] Trim
+    XMTN = np.zeros(NOVEL)  # [m] X-pos. of the motion coordinate system (relative to Lpp/2)
+    ZMTN = np.zeros(NOVEL)  # [m] Z-pos. of the mo􀆟on coordinate system (relative to BL)
+
+    # Initialize Force components
+    # Real parts
+    REFORCE = np.zeros([NDOF, NOFREQ, NOHEAD, NOVEL])  # Real part
+    IMFORCE = np.zeros([NDOF, NOFREQ, NOHEAD, NOVEL])  # Imaginary part
+
+    for i in range(NOVEL):
+        VEL[i], SINK[i], TRIM[i], XMTN[i], ZMTN[i] = [float(m) for m in f.readline().split()]
+        for j in range(NOHEAD):
+            HEAD[j] = float(f.readline())  # Should only contain one element
+            for k in range(NOFREQ):
+                FREQ[k] = float(f.readline())  # Should only contain one element
+
+                for m in range(NDOF):
+                    REFORCE[m, k, j, i], IMFORCE[m, k, j, i] = [float(m) for m in f.readline().split()][1:]
+
+    return REFORCE, IMFORCE, VEL, HEAD, FREQ
+
+if __name__ == "__main__":
+    REFORCE, IMFORCE, VEL, HEAD, FREQ = read_re8_file('Input files/test_input.re8')
+
+    print(REFORCE[0, 0, 0, 0])
