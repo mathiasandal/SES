dc00898e701ccc658b0c5ca8f1999c79783344c6
mathiasandal <mathias.andal@gmail.com>
2021-10-06 10:59:11 +0200

    Did some testing in Testing script and main

--- Testing script.py
+++ Testing script.py
@@ -1,7 +1,8 @@
 import numpy as np
+from Wave_response_utilities import decouple_matrix
 
-A = np.array([[1, 5], [2, 3]])
-
+A = np.array([[1., 5., 8.], [2., 3., 16.], [32., 54., 90.]])
+'''
 print(A)
 print()
 print(np.linalg.norm(np.transpose(A), ord=2, axis=1))
@@ -9,3 +10,10 @@ print()
 print(np.power(np.linalg.norm(np.transpose(A), ord=2, axis=1), -1))
 print()
 print(np.diag(np.power(np.linalg.norm(np.transpose(A), ord=2, axis=1), -1)))
+'''
+
+M = decouple_matrix(A, [1, 2])
+
+print(A)
+print()
+print(M)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from read_re8_file import read_re8_file
 from read_re7_file import read_re7_file
 from Wave_response_utilities import solve_eq_motion_steady_state
+
+import matplotlib.pyplot as plt
 import scipy.linalg as la
 import numpy as np
 
@@ -18,6 +20,34 @@ FORCE = REFORCE + 1j*IMFORCE
 # Gets the complex amplitude of the response eta_j, j = 1,...,6
 eta = solve_eq_motion_steady_state(VMAS + ADDMAS[0, 0, 0, :, :], DAMP[0, 0, 0, :, :], REST[0, 0, 0, :, :], FORCE[:, 0, 0, 0], FREQ1[0])
 
-D, V = la.eig(REST[0, 0, 0, :, :], VMAS + ADDMAS[0, 0, 0, :, :])
+D, V = la.eig(REST[0, 0, 14, :, :], VMAS + ADDMAS[0, 0, 14, :, :])
 
-print(np.round(np.abs(V), 3))
+print(np.round(REST[0, 0, 14, :, :], 1))
+
+'''
+print(np.sqrt(D[2]))
+print()
+print(np.round(V, 2))
+print()
+print(np.round(np.abs(V), 2))
+'''
+
+# Plotting coefficients
+plt.plot(FREQ1, REST[0, 0, :, 2, 2], 'x-')
+plt.xlabel("omega rad/s")
+plt.ylabel("C_33")
+plt.grid()
+plt.show()
+'''
+plt.plot(FREQ1, DAMP[0, 0, :, 2, 2], 'x-')
+plt.xlabel("wave frequency [rad/s]")
+plt.ylabel("B_33")
+plt.grid()
+plt.show()
+
+plt.plot(FREQ1, REFORCE[2, :, 0, 0], 'x-')
+plt.xlabel("wave frequency [rad/s]")
+plt.ylabel("Force in heave [N/m]")
+plt.grid()
+plt.show()
+'''
