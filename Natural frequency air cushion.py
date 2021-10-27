72193b77d6226deb9be70d085eb47861c85a5e09
mathiasandal <mathias.andal@gmail.com>
2021-10-27 22:03:31 +0200

    Applied functions for fan characteristics and fixed some inputs.

--- Natural frequency air cushion.py
+++ Natural frequency air cushion.py
@@ -1,16 +1,28 @@
 import numpy as np
+from air_cushion import air_cushion_area, interpolate_fan_characteristics, read_fan_characteristics #, find_closest_value
 
 # TODO: Make sure that the input parameters are correct
 
 """Input parameters"""
-A_b = 60  # [m^2] Air cushion area
-h_b = 0.4  # [m] Distance from waterline to the roof of the air cushion
-M = 25e3  # [kg] Total mass of the vessel
+l_rect = 9  # [m] length of the rectangular part of the air cushion
+l_tri = 10  # [m] length of the triangular part of the air cushion
+b_c = 4  # [m] beam of the air cushion
+h_b = 0.3  # [m] Distance from waterline to the roof of the air cushion #TODO Need to find out what this is.
+M = 25.6e3  # [kg] Total mass of the vessel
 p_0 = 3500  # [Pa] Excess air pressure in air cushion
 p_a = 101325  # [Pa] Atmospheric pressure
 gamma = 1.4  # [-] Ratio of specific heat for air
-Q_0 = 50  # [m^3/s] Mean volumetric flow of air fan
-dQdp_0 = -7.782e-3  # [(m^3s^-1)/(Pa)] Slope of fan characteristic curve estimated from Figure 5.6 in Faltinsen High-Speed
+
+# Read in fan characteristics
+Q, P, rpm_dummy = read_fan_characteristics('Input files/fan characteristics/fan characteristics.csv', '1800rpm')
+
+# Interpolates values
+Q_0, dQdp_0 = interpolate_fan_characteristics(p_0, P, Q)
+
+# Q_0 [m^3/s] Mean volumetric flow of air fan
+# dQdp_0 [(m^3s^-1)/(Pa)] Slope of fan characteristic curve estimated from Figure 5.6 in Faltinsen High-Speed
+
+A_b, x_c = air_cushion_area(l_rect, l_tri, b_c)
 
 '''
 Equivalent mass, damping and spring coefficients
